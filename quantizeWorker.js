2e0286299012b045b848fefb9d75d64e007f41d3
Vince489 <vincentcornelius489@gmail.com>
2024-08-23 00:45:09 -0400

    hi

--- quantizeWorker.js
+++ quantizeWorker.js
@@ -1,73 +1,29 @@
-self.onmessage = function(e) {
-    const { pixels, k } = e.data;
-    const quantizedPixels = kMeansQuantize(pixels, k);
-    self.postMessage(quantizedPixels);
+self.onmessage = function(event) {
+    const { imageData, numColors } = event.data;
+    const pixels = [];
+    for (let i = 0; i < imageData.data.length; i += 4) {
+        const r = imageData.data[i];
+        const g = imageData.data[i + 1];
+        const b = imageData.data[i + 2];
+        pixels.push([r, g, b]);
+    }
+
+    const colors = quantizeColors(pixels, numColors);
+    self.postMessage({ colors });
 };
 
-function kMeansQuantize(pixels, k) {
-    let centroids = [];
-    for (let i = 0; i < k; i++) {
-        centroids.push(pixels[Math.floor(Math.random() * pixels.length)]);
+function quantizeColors(pixels, numColors) {
+    const colorCounts = {};
+    for (const [r, g, b] of pixels) {
+        const key = `${r},${g},${b}`;
+        if (!colorCounts[key]) colorCounts[key] = 0;
+        colorCounts[key]++;
     }
 
-    let clusters = Array.from({ length: k }, () => []);
-    let oldCentroids = [];
-    let maxIterations = 10;
-    let iteration = 0;
+    const sortedColors = Object.entries(colorCounts)
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, numColors)
+        .map(([key]) => key.split(',').map(Number));
 
-    while (!areCentroidsEqual(centroids, oldCentroids) && iteration < maxIterations) {
-        clusters = Array.from({ length: k }, () => []);
-        for (const pixel of pixels) {
-            let minDistance = Infinity;
-            let clusterIndex = 0;
-            for (let i = 0; i < centroids.length; i++) {
-                const distance = getDistance(pixel, centroids[i]);
-                if (distance < minDistance) {
-                    minDistance = distance;
-                    clusterIndex = i;
-                }
-            }
-            clusters[clusterIndex].push(pixel);
-        }
-
-        oldCentroids = centroids.map(centroid => [...centroid]);
-        centroids = clusters.map(cluster => {
-            if (cluster.length === 0) return [0, 0, 0];
-            const sum = cluster.reduce((acc, pixel) => [acc[0] + pixel[0], acc[1] + pixel[1], acc[2] + pixel[2]], [0, 0, 0]);
-            return [Math.floor(sum[0] / cluster.length), Math.floor(sum[1] / cluster.length), Math.floor(sum[2] / cluster.length)];
-        });
-
-        iteration++;
-    }
-
-    return pixels.map(pixel => {
-        let minDistance = Infinity;
-        let nearestCentroid = centroids[0];
-        for (const centroid of centroids) {
-            const distance = getDistance(pixel, centroid);
-            if (distance < minDistance) {
-                minDistance = distance;
-                nearestCentroid = centroid;
-            }
-        }
-        return nearestCentroid;
-    });
-}
-
-function getDistance(color1, color2) {
-    return Math.sqrt(
-        Math.pow(color1[0] - color2[0], 2) +
-        Math.pow(color1[1] - color2[1], 2) +
-        Math.pow(color1[2] - color2[2], 2)
-    );
-}
-
-function areCentroidsEqual(centroids1, centroids2) {
-    if (centroids1.length !== centroids2.length) return false;
-    for (let i = 0; i < centroids1.length; i++) {
-        if (!centroids1[i].every((value, index) => value === centroids2[i][index])) {
-            return false;
-        }
-    }
-    return true;
+    return sortedColors;
 }
